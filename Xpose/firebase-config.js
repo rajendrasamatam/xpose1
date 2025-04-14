d02d85d1a1b1791212d93ced9298d3325741d5b0
rajendrasamatam <rajendrasamatam@gmail.com>
2025-04-14 23:15:36 +0530

    Update firebase-config.js

--- Xpose/firebase-config.js
+++ Xpose/firebase-config.js
@@ -3,15 +3,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Firebase configuration (Replace with your actual credentials)
-const firebaseConfig = {
-    apiKey: "AIzaSyDCCTnqO99HZ7Aw0jjn5AekOC12QX2LH5Q",
-    authDomain: "xpose-4452f.firebaseapp.com",
-    projectId: "xpose-4452f",
-    storageBucket: "xpose-4452f.firebasestorage.app",
-    messagingSenderId: "326293598496",
-    appId: "1:326293598496:web:07ca208569bd132c436745",
-    measurementId: "G-YD7RP953G5"
-  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
